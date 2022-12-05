8aa20b2bd86f4367582ba2e1ca2b8ffc827f342f
rodrigospicker1 <r.spicker13@gmail.com>
2022-12-05 14:07:03 -0300

    create_model_1

---
+++ application/controllers/Core_model.php
@@ -0,0 +1,103 @@
+<?php
+
+defined('BASEPATH') OR exit ('Ação não permitida');
+
+class Core_model extends CI_Model{
+
+
+	public function get_all($tabela = NULL, $condicao = NULL){
+
+		if($tabela){
+			if(is_array($codicao)){
+				$this->db->reset_query();
+				$this->db->where($condicao);
+			}
+			return $this->db->get($tabela)->result();
+		}else{
+			return FALSE;
+		}
+
+	}
+
+	public function get_by_id($tabela = NULL, $condicao = NULL){
+
+		if($tabela && is_array($condicao)){
+			
+			$this->db->where($condicao);
+			$this-<db->limit(1);
+
+			return $this->db->get($tabela)->row();
+
+		}else{
+			return FALSE;
+		}
+
+	}
+
+	public function insert($tabela = NULL, $data = NULL, $get_last_id = NULL){
+		if($tabela && is_array($data)){
+
+			$this->db->insert($tabela, $data);
+
+			if($get_last_id){
+				$this->session->set_userdata('last_id', $this->db->insert_id());
+			}
+
+			if($this->db->affected_rows() > 0){
+				$this->session->set_flashdata('sucesso', 'Dados salvos com sucesso');
+			}else{
+				$this->session->set_flashdata('error', 'Erro ao salvar dados');
+			}
+		}
+	}
+
+	public function update($tabela = NULL, $data = NULL, $codicao = NULL){
+
+		if($tabela && is_array($data) && is_array($condicao)){
+
+			if ($this->db->update($tabela, $data, $condicao)) {
+				$this->session->set_flashdata('sucesso', 'Dados salvos com sucesso');
+			}else{
+				$this->session->set_flashdata('error', 'Erro ao salvar dados');
+			}
+
+		}else{
+			return FALSE;
+		}
+
+	}
+
+	public function delete($abela = NULL, $condicao = NULL){
+
+		$this->db->db_debug = FALSE;
+
+		if($tabela && is_array($condicao)){
+
+			$status = $this->db->delete($tabela, $condicao);
+			$error = $this->db->error();
+
+			if(!$status){
+
+				foreach($error as $code){
+
+					if($code == 1451){
+
+						$this->session->set_flashdata('error', 'Esse registro não poderá ser excluído, pois esta sendo utilizado em outra tabela');
+
+					}
+
+				}
+
+			}else{
+				$this->session->set_flashdata('sucesso', 'Registro excluído com sucesso');
+			}
+
+			$this->db->db_debug = TRUE;
+
+		}else{
+			return FALSE;
+		}
+
+	}
+
+}

4fc1017b2349084fb66960645551c3af0f9710aa
rodrigospicker1 <r.spicker13@gmail.com>
2022-12-16 23:58:24 -0300

    login_html

---
+++ application/views/login/index.php
@@ -0,0 +1,41 @@
+    <div class="container">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+
+            <div class="col-xl-10 col-lg-12 col-md-9">
+
+                <div class="card o-hidden border-0 shadow-lg my-5" style="margin-top:10rem !important ;">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Seja bem-vindo!</h1>
+                                    </div>
+                                    <form class="user" name="form_index">
+                                        <div class="form-group">
+                                            <input type="email" name="email" class="form-control form-control-user"
+                                                placeholder="Entre com seu email...">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" name="password" class="form-control form-control-user" placeholder="Entre com sua senha">
+                                        </div>
+
+                                        <button href="<?= base_url('login'); ?>" type="submit" class="btn btn-primary btn-user btn-block">
+                                            Entrar
+                                        </button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>

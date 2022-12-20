bf734de3685eeffa32e62ab6437058b6e849e976
rodrigospicker1 <r.spicker13@gmail.com>
2022-12-19 23:28:35 -0300

    cliente_edit

--- application/controllers/Clientes.php
+++ application/controllers/Clientes.php
@@ -37,4 +37,61 @@ class Clientes extends CI_Controller {
 
 	}
 
+	public function edit($cliente_id = NULL){
+
+		if(!$cliente_id || !$this->core_model->get_by_id('clientes', array('cliente_id' => $cliente_id))){
+
+			$this->session->set_flashdata('error', 'Cliente não encontrado');
+
+		}else{
+
+			$this->form_validation->set_rules('cliente_nome', 'nome', 'trim|required|min_length[4]|max_length[45]');
+			$this->form_validation->set_rules('cliente_sobrenome', 'sobrenome', 'trim|required|min_length[4]|max_length[150]');
+			$this->form_validation->set_rules('cliente_data_nascimento', 'data de nascimento', 'required');
+			$this->form_validation->set_rules('cliente_cpf_cnpj', 'cpf ou cnpj', 'required|exact_length[18]');
+			$this->form_validation->set_rules('cliente_rg_ie', 'inscrição estadual', 'trim|required|max_length[20]');
+			$this->form_validation->set_rules('cliente_email', 'email', 'trim|required|max_length[50]');
+			$this->form_validation->set_rules('cliente_telefone', 'telefone', 'trim|max_length[14]');
+			$this->form_validation->set_rules('cliente_celular', 'celular', 'trim|max_length[15]');
+			$this->form_validation->set_rules('cliente_cep', 'cep', 'trim|required|exact_length[9]');
+			$this->form_validation->set_rules('cliente_endereco', 'endereço', 'trim|required|max_length[155]');
+			$this->form_validation->set_rules('cliente_numero_endereco', 'numero', 'trim|max_length[20]');
+			$this->form_validation->set_rules('cliente_bairro', 'bairro', 'trim|required|max_length[45]');
+			$this->form_validation->set_rules('cliente_complemento', 'complemento', 'trim|max_length[145]');
+			$this->form_validation->set_rules('cliente_cidade', 'cidade', 'trim|required|max_length[50]');
+			$this->form_validation->set_rules('cliente_estado', 'estado', 'trim|required|max_length[2]');
+			$this->form_validation->set_rules('cliente_obs', 'observações', 'max_length[500]');
+
+			if($this->form_validation->run()){
+
+				echo '<pre>';
+				print_r($this->input->post());
+				exit();
+
+			}else{
+
+				$data = array(
+
+					'titulo' => 'Atualizar cliente',
+
+					'cliente' => $this->core_model->get_by_id('clientes', 
+						array(
+							'cliente_id' => $cliente_id
+						))			
+				);
+
+				echo '<pre>';
+				print_r($data['cliente']);
+				exit();
+
+				$this->load->view('layout/header', $data);
+				$this->load->view('clientes/edit');
+				$this->load->view('layout/footer');
+
+			}
+
+		}
+
+	}
+
 }
